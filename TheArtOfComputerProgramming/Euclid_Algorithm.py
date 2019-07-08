30c2735df3f4fc49c3d70747f7d4078fbb4cb6dd
Ibtihaj Tahir <ibtihajtahir01@gmail.com>
2019-07-08 11:41:24 +0500

    Added The Art of Computer Programming First Algorithm

---
+++ TheArtOfComputerProgramming/Euclid_Algorithm.py
@@ -0,0 +1,24 @@
+# Given 2 positive integers m and n, find their greatest common divisor
+# Largest positive integers that evenly divides both m and n
+
+# Algorithm
+# E0. [Ensure m >= n.] if m < n, exchange(m, n)
+# E1. [Find Remainder.] Divide m by n
+# E2. [Is it zero?] if r == 0, return n
+# E3. [Reduce.] Set m = n, n = r and rpeate
+
+
+def euclid(m, n):
+
+    if m < n:  # Ensure m >= n
+        m, n = n, m  # If not, swap
+
+    r = m % n  # Find the remainder of m divided by n
+
+    if r == 0:  # If remainder is 0 Return number
+        return n
+    else:
+        return(euclid(n, r))  # Reduce. Update m by n and n by r
+
+
+print(euclid(119, 544))
